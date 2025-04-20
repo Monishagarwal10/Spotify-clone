db0ea0e160c7700184415235d3110aeab6597f0f
Monishagarwal10 <164761547+Monishagarwal10@users.noreply.github.com>
2025-04-20 16:40:51 +0530

    Plabar and seekbar all set

--- script.js
+++ script.js
@@ -18,17 +18,29 @@ async function getSongs() {
     return songs; 
 }
 
-const  playMusic = (track)=>{
-    // let audio = new Audio("/songs/" + track)
+const  playMusic = (track,pause=false)=>{
     currentSong.src = "/songs/" + track
-    currentSong.play();
-    play.src = "pause.png"
+    // let audio = new Audio("/songs/" + track)
+    if(!pause){
+        currentSong.play();
+        play.src = "pause.svg"
+    }
+    document.querySelector(".songName").innerHTML = decodeURI(track)
+    // document.querySelector(".songTimeMin").innerHTML = "00:00/00:00"
 
 }
 
+function secondsToMinutesSeconds(seconds) {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = Math.floor(seconds % 60);
+    const formattedSeconds = remainingSeconds < 10 ? `0${remainingSeconds}` : remainingSeconds;
+    return `${minutes}:${formattedSeconds}`;
+}
+
+
 async function main() {
     let songs = await getSongs()
-    console.log(songs);//get the list of all songs
+    playMusic(songs[0] , true)// Helps to play the first song when u randomly click on play 
 
     let songUl = document.querySelector(".songList").getElementsByTagName("ul")[0]
     for (const song of songs) {
@@ -52,14 +64,6 @@ async function main() {
     e.addEventListener("click",elements=>{
         console.log(e.querySelector(".songInfo").firstElementChild.innerHTML)
         playMusic(e.querySelector(".songInfo").firstElementChild.innerHTML.trim())
-        // if(currentSong.paused){
-        //     currentSong.play()
-        //     play.src = "pause-button.png"
-        // }
-        // else{
-        //     currentSong.pause()
-        //     play.src = "play.png"
-        // }
     })
     })
 
@@ -67,14 +71,33 @@ async function main() {
     play.addEventListener("click",()=>{
         if(currentSong.paused){
             currentSong.play()
-            play.src = "pause.png"
+            play.src = "pause.svg"
         }
         else{
             currentSong.pause()
-            play.src = "playbar.png"
+            play.src = "play.svg"
         }
     })
 
+    //To display the current time of the song
+        currentSong.addEventListener("timeupdate" , ()=>{
+        // console.log(currentSong.currentTime , currentSong.duration);
+        document.querySelector(".songTimeMin").innerHTML = 
+        `${secondsToMinutesSeconds(currentSong.currentTime)}`
+        document.querySelector(".songDuration").innerHTML = 
+        `${secondsToMinutesSeconds(currentSong.duration)}`;
+        document.querySelector(".circle").style.left =
+         (currentSong.currentTime/currentSong.duration) * 100 + "%";
+    });
+
+    //Handle the seekbar
+    document.querySelector(".seekbar").addEventListener("click",e=>{
+        let percent = (e.offsetX/e.target.getBoundingClientRect().width)*100;
+        document.querySelector(".circle").style.left = percent + "%"
+         currentSong.currentTime = ((currentSong.duration)* percent) / 100;
+         document.querySelector(".seekbar").style.background = `linear-gradient(to right, #1ed760 ${percent}%, #ccc ${percent}%)`;
+
+    })
     
 }
 
